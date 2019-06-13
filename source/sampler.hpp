019c7a5e9c680283bc15e39b4559cc21ca5939a7
pchdev <pchd@quarre.fr>
2019-06-13 16:28:58 +0200

    wip on 1.1

--- source/sampler.hpp
+++ source/sampler.hpp
@@ -3,43 +3,51 @@
 #include <wpn114audio/graph.hpp>
 #include <sndfile.h>
 
-#define WPN_DECLARE_AUDIO_INPUT(_name, _nchannels) \
-    WPN_DECLARE_AUDIO_PORT(_name, Polarity::Input, _nchannels)
+#define CSTR(_qstr) _qstr.toStdString().c_str()
 
-#define WPN_DECLARE_AUDIO_OUTPUT(_name, _nchannels) \
-    WPN_DECLARE_AUDIO_PORT(_name, Polarity::Output, _nchannels)
-
+//=================================================================================================
 class Sampler : public Node
+//=================================================================================================
 {
     Q_OBJECT
 
+    //---------------------------------------------------------------------------------------------
+    WPN_DECLARE_DEFAULT_MIDI_INPUT(midi_in, 1)
+
+    //---------------------------------------------------------------------------------------------
     WPN_DECLARE_AUDIO_INPUT(play, 1)
     WPN_DECLARE_AUDIO_INPUT(loop, 1)
     WPN_DECLARE_AUDIO_INPUT(rate, 1)
     WPN_DECLARE_AUDIO_INPUT(startpos, 1)
     WPN_DECLARE_AUDIO_INPUT(endpos, 1)
 
+    WPN_DECLARE_AUDIO_INPUT(attack, 1)
+    WPN_DECLARE_AUDIO_INPUT(release, 1)
+    WPN_DECLARE_AUDIO_INPUT(xfade, 1)
+
+    //---------------------------------------------------------------------------------------------
+    WPN_DECLARE_DEFAULT_AUDIO_OUTPUT(audio_out, 0)
     WPN_DECLARE_AUDIO_OUTPUT(end_out, 1)
     WPN_DECLARE_AUDIO_OUTPUT(loop_out, 1)
-    WPN_DECLARE_AUDIO_OUTPUT(audio_out, 0)
 
+    Q_PROPERTY(QString path READ path WRITE setPath)
+
+    //---------------------------------------------------------------------------------------------
     SNDFILE* m_file         = nullptr;
     sample_t* m_envelope    = nullptr;
-    sample_t** m_buffer     = nullptr;
+    sample_t* m_buffer      = nullptr; // interleaved
     SF_INFO m_info;
     sample_t m_srate = 0;
     sample_t m_bufplayrate = 0;
+    QString m_path;
 
-    uint32_t
-    m_attack    = 0,
-    m_release   = 0,
-    m_xfade     = 0;
-
+    //---------------------------------------------------------------------------------------------
     bool
     m_playing    = false,
     m_attacking  = false,
     m_releasing  = false;
 
+    //---------------------------------------------------------------------------------------------
     sample_t
     m_fphase = 0,
     m_aphase = 0,
@@ -47,33 +55,156 @@ class Sampler : public Node
     m_inc_a  = 0,
     m_inc_r  = 0;
 
+    static constexpr size_t
+    esz = 32768;
+
 public:
 
-    Sampler()
-    {
+    //---------------------------------------------------------------------------------------------
+    Sampler() {}
 
+    //---------------------------------------------------------------------------------------------
+    virtual ~Sampler() override
+    //---------------------------------------------------------------------------------------------
+    {
+        delete[] m_envelope;
+        delete[] m_buffer;
     }
 
-    ~Sampler()
-    {
+    //---------------------------------------------------------------------------------------------
+    QString
+    path() const { return m_path; }
 
+    //---------------------------------------------------------------------------------------------
+    void
+    set_path(QString path) { m_path = path; }
+
+    //---------------------------------------------------------------------------------------------
+    virtual QString
+    name() const override { return "Sampler"; }
+
+    //---------------------------------------------------------------------------------------------
+    virtual void
+    componentComplete()
+    // this happens before allocation
+    // load file, set n_outputs
+    //---------------------------------------------------------------------------------------------
+    {
+        if (m_path.isEmpty())
+            return;
+
+        m_file = sf_open(CSTR(m_path), SFM_READ, &m_info);
+        m_audio_out.set_nchannels(m_info.channels);
+        Node::componentComplete();
     }
 
+    //---------------------------------------------------------------------------------------------
     virtual void
     initialize(const Graph::properties& properties) override
+    // allocate envelope and buffer
+    // setup playing rates, envelope size in frames, etc.
+    //---------------------------------------------------------------------------------------------
     {
+        m_srate     = properties.rate;
+        m_envelope  = new sample_t[esz]();
+        m_buffer    = new sample_t[m_info.frames*m_info.channels]();
+        // note: buffer is interleaved
 
+        // fill envelope
+        for (size_t n = 0; n < esz; ++n)
+            m_envelope[n] = sin(M_PI_4*static_cast<sample_t>(n)/esz);
+
+        // read buffer
+        sf_readf_double(m_file, m_buffer, m_info.frames);
     }
 
+    //---------------------------------------------------------------------------------------------
     virtual void
     on_rate_changed(sample_t rate) override
+    //---------------------------------------------------------------------------------------------
     {
-
+        m_srate = rate;
     }
 
+    //---------------------------------------------------------------------------------------------
     virtual void
-    rwrite(pool &inputs, pool &outputs, vector_t nframes) override
+    rwrite(pool& inputs, pool& outputs, vector_t nframes) override
+    //---------------------------------------------------------------------------------------------
     {
+        auto play       = inputs[0][0];
+        auto loop       = inputs[1][0];
+        auto rate       = inputs[2][0];
+        auto startpos   = inputs[3][0];
+        auto endpos     = inputs[4][0];
+        auto attack     = inputs[5][0];
+        auto release    = inputs[6][0];
+        auto xfade      = inputs[7][0];
+
+        auto out        = outputs[0];
+        auto end_out    = outputs[1][0];
+        auto loop_out   = outputs[2][0];
+
+        auto playing    = m_playing;
+        auto attacking  = m_attacking;
+        auto releasing  = m_releasing;
+
+        auto fphase     = m_fphase;
+        auto aphase     = m_aphase;
+        auto rphase     = m_rphase;
+        auto inc_a      = m_inc_a;
+        auto inc_r      = m_inc_r;
+
+        for (vector_t f = 0; f < nframes; ++f)
+        {
+            bool sigplay = play[f] != 0.;
+
+            if (!playing && sigplay)
+            {
+
+            }
+
+            else if (!playing && sigplay)
+            {
+
+            }
+
+            else if (playing && sigplay)
+            {
+
+            }
+
+            sample_t frame[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            uint32_t endpos_f = endpos[f];
+            uint32_t startpos_f = startpos[f];
+
+            if (fphase >= endpos_f || fphase < startpos_f)
+            {
+
+            }
+
+            // fetch data from buffer
+
+            // attack/release/xfade ratios
+
+            if (attacking)
+            {
+
+            }
+
+            if (xfade >= xfade_point)
+            {
+
+            }
+
+            if (releasing)
+            {
+
+            }
+
+
+
+        }
+
 
     }
 
